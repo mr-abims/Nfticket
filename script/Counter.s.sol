f6ef65ef4a988da3eda0bf2adc113f26ecdef2d2
mr-abims <adebayoabimbola9@gmail.com>
2025-09-17 19:10:51 +0100

    script fix

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,21 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import {Counter} from "../src/Counter.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {EventManager} from "../src/EventManager.sol";
 
-// contract CounterScript is Script {
-//     Counter public counter;
+contract CounterScript is Script {
+    EventManager public eventManager;
 
-//     function setUp() public {}
+    function setUp() public {
+        eventManager = new EventManager( "Test Event", "TE", block.timestamp + 100, block.timestamp + 1000, 0, 100, "ipfs://testuri", address(this));
+    }
 
-//     function run() public {
-//         vm.startBroadcast();
+    function run() public {
+        vm.startBroadcast();
 
-//         counter = new Counter();
+        eventManager = new EventManager( "Test Event", "TE", block.timestamp + 100, block.timestamp + 1000, 0, 100, "ipfs://testuri", address(this));
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
